0a4d7935ba84f6566170138ad6b834696877dc35
ZeloZalis <117476567+ZeloZalis@users.noreply.github.com>
2023-11-07 17:08:47 -0400

    Update
    Added new functions for the menu.

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from lib.GET_api import *
+from lib.GET import *
+from lib.functions import *
+
+options = [
+    '\nBienvenido al menú interactivo de la Pokédex GO, ¿qué desea realizar?',
+    '1. Revisar el estado de las tablas.',
+    '2. Mostrar la lista de Pokémon.',
+    '3. Buscar un Pokémon.',
+    '4. Buscar un ataque rápido/cargado.',
+    '5. Ver la lista de ataques rápidos.',
+    '6. Ver la lista de ataques cargados.',
+    '7. Cargar las tablas de la base de datos.',
+    '8. Resetear las tablas de la base de datos.'
+]
+
+while True:
+    for n in options:
+        print(n)
+    select = int(input(''))
