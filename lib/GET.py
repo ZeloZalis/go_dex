86ff1f8949b5262467322939402710b207f41a04
ZeloZalis <117476567+ZeloZalis@users.noreply.github.com>
2023-11-28 20:15:58 -0400

    Last Update/Translation
    Fixed all the functions and translated the app to english.

--- lib/GET.py
+++ lib/GET.py
@@ -4,14 +4,18 @@ import psycopg2
 from decouple import config
 
 def call_db():
-    con = psycopg2.connect(
-        user = config('SQL_user'),
-        password = config('SQL_password'),
-        host = config('SQL_host'),
-        port = config('SQL_port'),
-        database = config('SQL_database')
-    )
-    return con
+    try:
+        con = psycopg2.connect(
+            user = config('SQL_user'),
+            password = config('SQL_password'),
+            host = config('SQL_host'),
+            port = config('SQL_port'),
+            database = config('SQL_database')
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        return con
 
 def Create_table():
     con = call_db()
@@ -19,19 +23,19 @@ def Create_table():
         cursor = con.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS pokemon (id SERIAL PRIMARY KEY, dexnum INTEGER UNIQUE NOT NULL, name TEXT NOT NULL, type1 TEXT NOT NULL, type2 TEXT, atk INTEGER NOT NULL, hp INTEGER NOT NULL, def INTEGER NOT NULL)""")
         con.commit()
-        print("Tabla pokemon creada.")
+        print("'Pokemon' table created.")
         cursor.execute("""CREATE TABLE IF NOT EXISTS fast_moves (id SERIAL PRIMARY KEY, name TEXT NOT NULL, type TEXT NOT NULL, power INTEGER NOT NULL, energy INTEGER NOT NULL, speed INTEGER NOT NULL)""")
         con.commit()
-        print("Tabla fast_moves creada.")
+        print("'Fast_moves' table created.")
         cursor.execute("""CREATE TABLE IF NOT EXISTS charged_moves (id SERIAL PRIMARY KEY, name TEXT NOT NULL, type TEXT NOT NULL, power INTEGER NOT NULL, energy INTEGER NOT NULL)""")
         con.commit()
-        print("Tabla charged_moves creada.")
+        print("'Charged_moves' table created.")
         cursor.execute("""CREATE TABLE IF NOT EXISTS pokemon_fast_moves (id_pokemon INT NOT NULL, id_fast_moves INT NOT NULL, PRIMARY KEY (id_pokemon, id_fast_moves), FOREIGN KEY (id_pokemon) REFERENCES pokemon (id), FOREIGN KEY (id_fast_moves) REFERENCES fast_moves (id))""")
         con.commit()
-        print("Tabla pokemon_fast_moves creada.")
+        print("'Pokemon_fast_moves' table created.")
         cursor.execute("""CREATE TABLE IF NOT EXISTS pokemon_charged_moves (id_pokemon INT NOT NULL, id_charged_moves INT NOT NULL, PRIMARY KEY (id_pokemon, id_charged_moves), FOREIGN KEY (id_pokemon) REFERENCES pokemon (id), FOREIGN KEY (id_charged_moves) REFERENCES charged_moves (id))""")
         con.commit()
-        print("Tabla pokemon_charged_moves creada.")
+        print("'Pokemon_charged_moves' table created.")
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -47,11 +51,11 @@ def fast_moves_upload():
         for n in data:
             record_to_insert = (n['name'], n['type'], n['power'], n['energy_delta'], n['duration'])
             cursor.execute(insert_query, record_to_insert)
-            print(f"Se ha cargado {n['name']} a la base de datos.")
+            print(f"{n['name']} added.")
         con.commit()
-        print('\n---------------------------------------------------------------------------')
-        print('Se han agregado todos los ataques rápidos a la base de datos correctamente.')
-        print('---------------------------------------------------------------------------\n')
+        print('\n-----------------------------------------------')
+        print('All fast moves has been loaded to the database.')
+        print('-----------------------------------------------\n')
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -67,11 +71,11 @@ def charged_moves_upload():
         for n in data:
             record_to_insert = (n['name'], n['type'], n['power'], n['energy_delta'])
             cursor.execute(insert_query, record_to_insert)
-            print(f"Se ha cargado {n['name']} a la base de datos.")
+            print(f"{n['name']} added.")
         con.commit()
-        print('\n----------------------------------------------------------------------------')
-        print('Se han agregado todos los ataques cargados a la base de datos correctamente.')
-        print('----------------------------------------------------------------------------\n')
+        print('\n--------------------------------------------------')
+        print('All charged moves has been loaded to the database.')
+        print('--------------------------------------------------\n')
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -93,16 +97,16 @@ def pokemon_upload():
                             query = """INSERT INTO pokemon (dexnum, name, type1, atk, hp, def) VALUES (%s, %s, %s, %s, %s, %s)"""
                             record_insert = (n['pokemon_id'], n['pokemon_name'], k['type'][0], n['base_attack'], n['base_stamina'], n['base_defense']) 
                             cursor.execute(query, record_insert)
-                            print(f"{n['pokemon_name']} ha sido cargado correctamente.")
+                            print(f"{n['pokemon_name']} added.")
                         elif len(k['type']) == 2:
                             query = """INSERT INTO pokemon (dexnum, name, type1, type2, atk, hp, def) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
                             record_insert = (n['pokemon_id'], n['pokemon_name'], k['type'][0], k['type'][1], n['base_attack'], n['base_stamina'], n['base_defense'])
                             cursor.execute(query, record_insert)
-                            print(f"{n['pokemon_name']} ha sido cargado correctamente.")
+                            print(f"{n['pokemon_name']} added.")
         con.commit()
-        print('\n-------------------------------------------------------------------')
-        print('Se han agregado todos los Pokémon a la base de datos correctamente.')
-        print('-------------------------------------------------------------------\n')
+        print('\n------------------------------------------')
+        print('All Pokémon has been loaded to the database.')
+        print('------------------------------------------\n')
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -125,11 +129,11 @@ def fk_table_upload_fast_moves():
                         for m in fast_attack_list:
                             if i == m[1]:
                                 cursor.execute(f"INSERT INTO pokemon_fast_moves (id_pokemon, id_fast_moves) VALUES ({n[0]}, {m[0]}) ON CONFLICT (id_pokemon, id_fast_moves) DO NOTHING")
-                                print(f"Se ha cargado el ataque {i} en {n[2]}.")
+                                print(f"{i} has been added to {n[2]}.")
         con.commit()
-        print('\n------------------------------------------------------------------')
-        print('Se ha vinculado los ataques rápidos con los Pokémon correctamente.')
-        print('------------------------------------------------------------------\n')
+        print('\n----------------------------------------------------------------')
+        print('Fast moves have been linked to their respective Pokémon correctly.')
+        print('----------------------------------------------------------------\n')
     except Exception as e:
         print(f"Error: {e}")
     finally:
@@ -152,10 +156,10 @@ def fk_table_upload_charged_moves():
                         for m in charged_moves_list:
                             if i == m[1]:
                                 cursor.execute(f"INSERT INTO pokemon_charged_moves (id_pokemon, id_charged_moves) VALUES ({n[0]}, {m[0]}) ON CONFLICT (id_pokemon, id_charged_moves) DO NOTHING")
-                                print(f"Se ha cargado el ataque {i} en {n[2]}.")
+                                print(f"{i} has been added to {n[2]}.")
         con.commit()
         print('\n-------------------------------------------------------------------')
-        print('Se ha vinculado los ataques cargados con los Pokémon correctamente.')
+        print('Charged moves have been linked to their respective Pokémon correctly.')
         print('-------------------------------------------------------------------\n')
     except Exception as e:
         print(f"Error: {e}")
@@ -164,7 +168,7 @@ def fk_table_upload_charged_moves():
 
 def initialization():
     call_db()
-    print('Iniciando...\n')
+    print('Getting started...\n')
     fast_moves_upload()
     charged_moves_upload()
     pokemon_upload()
@@ -173,6 +177,6 @@ def initialization():
     print('.')
     print('..')
     print('...\n')
-    print('----------------------------------------------------------------')
-    print('Se han cargado todos los datos a la base de datos correctamente.')
-    print('----------------------------------------------------------------')
+    print('-----------------------------------------------------')
+    print('All data has been loaded into the database correctly.')
+    print('-----------------------------------------------------')
